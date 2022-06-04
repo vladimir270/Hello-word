03f613bc737273f75716ba130c5d5d90dcb7c908
Владимир Ворона <vvorona665@gmail.com>
2022-06-04 18:10:59 +0300

    1234

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int namberA = 31;
-int namberB = 5;
+﻿int namberA = new Random().Next(1,10);// 1,2,3,4....9,10
+Console.WriteLine(namberA);
+int namberB = new Random().Next(1,10);
+Console.WriteLine(namberB);
 int result = namberA + namberB;
 Console.WriteLine(result);
